e6737a569fe49713d7886c527a18fd72c0b43458
dhritiman.tamuli.saikia <dhritiman.tamuli.saikia@gmail.com>
2025-07-03 12:43:19 +0000

    changed put to post

--- mymongoproject/myapp/views.py
+++ mymongoproject/myapp/views.py
@@ -86,7 +86,7 @@ def lead_activity_view(request, lead_id):
             return HttpResponse('Lead not found', status=404)
         except Exception as e:
             return HttpResponse(str(e), status=400)
-    elif request.method == 'PUT':
+    elif request.method == 'POST':
         try:
             data = json.loads(request.body)
             lead = Lead.objects.get(id=lead_id)
